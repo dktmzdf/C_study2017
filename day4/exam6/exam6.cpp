dedd68d36217ae613d470490213ca09acd31b4a9
kujin <dktmzdf@gmail.com>
2017-06-26 14:30:40 +0900

    exam6

---
+++ day4/exam6/exam6.cpp
@@ -0,0 +1,14 @@
+// exam6.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("Hello World\n");
+	printf(" 10 \n");
+	printf(" %d \n", 10 + 20);
+    return 0;
+}
+
