53c09a6fcc6c2af72bdf5379d3abceb0e7290f05
kujin <dktmzdf@gmail.com>
2017-06-27 09:36:33 +0900

    exan1

---
+++ day5/exan1/exan1.cpp
@@ -0,0 +1,20 @@
+// exan1.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("%d \n", 12);
+	printf("%d \n", 014);
+	printf("%d \n", 0xc);
+	//printf("%d \n", b1100);
+
+	printf("%d \n", 12);
+	printf("%o \n", 014);
+	printf("%x \n", 0xc);
+
+    return 0;
+}
+
