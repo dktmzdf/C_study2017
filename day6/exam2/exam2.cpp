54f86f7d44b300f5762c42da79bc6ec9ba0945c5
kujin <dktmzdf@gmail.com>
2017-06-28 09:41:48 +0900

    exam2

---
+++ day6/exam2/exam2.cpp
@@ -0,0 +1,25 @@
+// exam2.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("-------------while---------------\n");
+	int nIndex=0;
+	while (nIndex > 10) {
+		
+		printf_s("%d \n",nIndex);
+		nIndex++;
+	}
+	nIndex = 0;
+	printf("-------------do while------------\n");
+	do{
+		printf_s("%d \n", nIndex);
+		nIndex++;
+
+	} while (nIndex > 10);
+    return 0;
+}
+
