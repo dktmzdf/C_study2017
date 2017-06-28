f8e3227d74c50dabbce5accc168c6391cf70482f
kujin <dktmzdf@gmail.com>
2017-06-28 15:53:12 +0900

    exam11

--- engine/mywin32_engine.h
+++ engine/mywin32_engine.h
@@ -1,19 +1,26 @@
 #pragma once
-void makeMiniEdit(HWND hWnd, int xPos, int yPos, int nHandle)
-{
-	CreateWindow(L"edit", NULL, WS_CHILD | WS_VISIBLE | CBS_AUTOHSCROLL | WS_BORDER,
-		xPos, yPos, 100, 25, hWnd, (HMENU)nHandle, hInst, NULL);
-}
-void makeMiniButton(HWND hWnd, int xPos, int yPos, int nHandle, TCHAR *pszLabel)
-{
+namespace mywin32_engine {
+	void makeMiniEdit(HWND hWnd, int xPos, int yPos, int nHandle)
+	{
+		CreateWindow(L"edit", NULL, WS_CHILD | WS_VISIBLE | CBS_AUTOHSCROLL | WS_BORDER,
+			xPos, yPos, 100, 25, hWnd, (HMENU)nHandle, hInst, NULL);
+	}
+	void makeMiniButton(HWND hWnd, int xPos, int yPos, int nHandle, TCHAR *pszLabel)
+	{
 
-	CreateWindow(L"button", pszLabel, WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-		xPos, yPos, 100, 25, hWnd, (HMENU)nHandle, hInst, NULL);
-}
-int GetControlValueInt(HWND hWnd, int nHandle) {
-	TCHAR szBuf[256];
-	GetWindowText(GetDlgItem(hWnd, nHandle), szBuf, 256);
-	int result = _wtoi(szBuf);
+		CreateWindow(L"button", pszLabel, WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			xPos, yPos, 100, 25, hWnd, (HMENU)nHandle, hInst, NULL);
+	}
+	int GetControlValueInt(HWND hWnd, int nHandle) {
+		TCHAR szBuf[256];
+		GetWindowText(GetDlgItem(hWnd, nHandle), szBuf, 256);
+		int result = _wtoi(szBuf);
 
-	return result;
+		return result;
+	}
+	void SetControlValueInt(HWND hWnd, int nId, int nValue) {
+		TCHAR szBuf[256];
+		swprintf_s(szBuf, 256, L"%d", nValue);
+		SetWindowText(GetDlgItem(hWnd, nId), szBuf);
+	}
 }
