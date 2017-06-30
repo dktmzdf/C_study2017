c7f9daf6f371267e1e17728c72b9c963149b0c5e
kujin <dktmzdf@gmail.com>
2017-06-30 15:09:03 +0900

    exam7

---
+++ day8/exam7/exam7.cpp
@@ -0,0 +1,39 @@
+// exam7.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+void assign(int *test)//포인터가 없으면 값을 복사함 (call of value) 포인터가 있으면 주소를 복사함 (call of reference)
+{
+	*test = 100;
+}
+
+void getSum(int a, int b, int *nSum)
+{
+	*nSum = a + b;
+}
+
+void exchange(int *a, int *b) {
+	int temp;
+	temp = *b;
+	*b = *a;
+	*a = temp;
+}
+
+int main()
+{
+	int test=0;
+
+	assign(&test);//주소를 넘겨줌
+	printf("%d \n", test);
+	getSum(5, 6, &test);
+	printf("%d \n",test);
+
+	int a, b;
+	a = 1;
+	b = 7;
+	exchange(&a,&b);
+	printf("%d %d\n", a,b);
+    return 0;
+}
+
