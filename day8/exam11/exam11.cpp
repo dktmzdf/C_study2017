dfa38d18c66d283543df985b3b7ada52146ddbeb
kujin <dktmzdf@gmail.com>
2017-06-30 16:25:37 +0900

    exam11

---
+++ day8/exam11/exam11.cpp
@@ -0,0 +1,28 @@
+// exam11.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+void getAvg(int pBuf[],int *result) 
+{
+	int nSum = 0;
+	for (int i = 0; i < 5; i++) 
+	{
+		nSum += pBuf[i];
+	}
+	*result = nSum / 5;
+	
+}
+
+int main()
+{
+	int buf[5] = { 3,5,6,7,8 };
+
+	int nAvg;
+
+	getAvg(buf, &nAvg);
+
+	printf("%d",nAvg);
+    return 0;
+}
+
