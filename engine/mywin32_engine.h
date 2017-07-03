d37e24ad30f5354423b5164dd84f5c076bde2667
kujin <dktmzdf@gmail.com>
2017-07-03 13:52:34 +0900

    exam6

--- engine/mywin32_engine.h
+++ engine/mywin32_engine.h
@@ -1,9 +1,34 @@
 #pragma once
 namespace mywin32_engine {
-	void makeMiniEdit(HWND hWnd, int xPos, int yPos, int nHandle)
+	namespace util {
+		//,로 구분된 문자열을 실수형 배열에 넣는다. 반환값은 생서오딘 실수의 개수
+		int Buffer2DoubleArray(TCHAR *szBuf,double *pResult)
+		{
+			int nCount = 0;
+			TCHAR szTemp[256];
+			TCHAR *pwc;
+			pwc = wcstok(szBuf, L", ");
+			pResult[nCount++] = _wtof(pwc);
+			while (pwc != NULL) {
+				pwc = wcstok(NULL, L", ");//NULL를 넣어주면 전에 넣어 줬던걸 계속 씀
+				if (pwc != NULL) {
+					pResult[nCount++] = _wtof(pwc);
+				}
+
+			}
+			return nCount;
+		}
+	}
+
+	void makeMiniEditBox(HWND hWnd, int xPos, int yPos, int nHandle)
 	{
-		CreateWindow(L"edit", NULL, WS_CHILD | WS_VISIBLE | CBS_AUTOHSCROLL | WS_BORDER,
+		CreateWindow(L"edit", NULL, WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL | WS_BORDER,
 			xPos, yPos, 100, 25, hWnd, (HMENU)nHandle, hInst, NULL);
+	}
+	void makeEditBox(HWND hWnd, int xPos, int yPos,int nWdith, int nHandle)
+	{
+		CreateWindow(L"edit", NULL, WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL | WS_BORDER,
+			xPos, yPos, nWdith, 25, hWnd, (HMENU)nHandle, hInst, NULL);
 	}
 	void makeMiniButton(HWND hWnd, int xPos, int yPos, int nHandle, TCHAR *pszLabel)
 	{
