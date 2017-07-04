56ec187ce0be7e6633146507b47aee8582f80b64
kujin <dktmzdf@gmail.com>
2017-07-04 13:25:44 +0900

    exam3

---
+++ day10/exam3/exam3.cpp
@@ -0,0 +1,21 @@
+// exam3.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	char *pTest = "123456789";
+	char Test2[64] = "123456789";
+
+	printf("%u %u\n",pTest, &Test2);
+
+	Test2[2] = '*';
+
+	printf("%s \n",Test2);
+
+	//TCHAR 유니코드 문자열
+    return 0;
+}
+
