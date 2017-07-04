a4b8fe7cf6cb3bd53e64b423d836aeb4a4795b47
kujin <dktmzdf@gmail.com>
2017-07-04 14:50:49 +0900

    exam7

--- day10/exam7/exam7.cpp
+++ day10/exam7/exam7.cpp
@@ -2,15 +2,34 @@
 //
 
 #include "stdafx.h"
-
+#include <string.h>
 
 int main()
 {
 	char szBuf[256];
 
-	gets_s(szBuf,64);
-	puts(szBuf);
+	//gets_s(szBuf,64);
+	//puts(szBuf);
 
+	char c;
+	//c = getchar();
+	//putchar(c);
+
+	char szTemp[64] = "apple[seed]tree";
+	int nBufSize =strlen(szTemp);
+	int nTempSize1 = strlen("apple");
+	int nTempSize2 = strlen("[seed]");
+	/*for (int i = 0; szTemp[nTempSize1 + nTempSize2 + i] != 0x00; i++) {
+		szTemp[nTempSize1 + i] = szTemp[nTempSize1 + nTempSize2 + i];
+	}
+	printf("%s\n",szTemp);*/
+	
+	strcpy((szTemp + nTempSize1), (szTemp + nTempSize1 + nTempSize2));
+	printf("%s\n", szTemp);
+	//////
+	szTemp[5] = 0x00;
+	strcat(szTemp, szTemp + 11);
+	printf("%s \n",szTemp);
     return 0;
 }
 
