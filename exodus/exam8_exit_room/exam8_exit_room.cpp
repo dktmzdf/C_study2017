cb24da5570f61b0364b0eb86f5be066c70e64acf
kujin <dktmzdf@gmail.com>
2017-07-05 15:45:53 +0900

    exam8_exit_room

---
+++ exodus/exam8_exit_room/exam8_exit_room.cpp
@@ -0,0 +1,329 @@
+// exam8_exit_room.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "exam8_exit_room.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+
+#include <windows.h>
+#include <objidl.h>
+#include <gdiplus.h>
+using namespace Gdiplus;
+#pragma comment (lib,"Gdiplus.lib")
+#include "../../engine/myGdiplusGame.h"
+using namespace myGdiplusGame;
+
+
+//y*8 + x 가 (x,y)위치
+int g_MapRoom1[] = {
+	3, 0, 0, 0, 0, 0, 0, 3,
+	1,14,14,14,14,14,14, 1,
+	1,14,14,14,14,14,14, 1,
+	1,14,14,14,14,14,14, 1,
+	1,14,14,14,14,14,14, 1,
+	1,14,14,14,14,14,14, 1,
+	1,14,14,14,14,14,14, 1,
+	2, 2, 2, 48, 2, 2, 2, 2
+};
+
+int *g_nowMap;
+int g_nPlayerXPos=3;
+int g_nPlayerYPos=3;
+
+DWORD g_dwGdiLoopFsm = 0; //루프상태제어
+void GDIPLUS_Loop(MSG &msg)
+{
+	//----------------------------------------------------------------------
+	//gdi plus 초기화 코드 
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR           gdiplusToken;
+
+	// Initialize GDI+.
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+	//-----------------------------------------------------------------------
+
+	{
+		bool quit = false;
+		//gdiplus 가 셧다운 되기전에 객체들이 삭제되어야 하므로 일부러 지역변수선언을 한단계 내려서 사용했다.
+		Gdiplus::Rect rectScreen(0, 0, 320, 240);
+		Bitmap bmpMem(rectScreen.Width, rectScreen.Height);
+		Graphics* graphBackBuffer = Graphics::FromImage(&bmpMem);
+
+		Pen penRed(Color(255, 0, 0));
+		Gdiplus::SolidBrush brushBlack(Color(0, 0, 0));
+		Gdiplus::SolidBrush brushWhite(Color(255, 255, 255));
+		FontFamily  fontFamily(L"굴림");
+		Font        font(&fontFamily, 12, FontStyleRegular, UnitPixel);
+		static LONG prev_tick;
+		static SYSTEMTIME time;
+
+		Image imgBasicTile(L"../../res/basic_tile/basictiles.png");//16 x 16;
+		Image imgPlayer(L"../../res/potrait/charater.png");//크기 x64 y64
+
+		g_nowMap = g_MapRoom1;
+		while (!quit) {
+
+			if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
+			{
+				if (msg.message == WM_QUIT)
+					quit = true;
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else {
+				switch (g_dwGdiLoopFsm)
+				{
+				case 0:
+					break;
+				case 10:
+				{
+					GetSystemTime(&time);
+					LONG time_ms = (time.wSecond * 1000) + time.wMilliseconds;
+					float fDelta = (time_ms - prev_tick) / 1000.f;
+					prev_tick = time_ms;
+					
+					// Get DC
+					HDC hdc = GetDC(msg.hwnd);
+					{
+						Graphics graphics(hdc);
+
+						graphBackBuffer->FillRectangle(&brushBlack, rectScreen);
+
+
+						//지도그리기
+						for (int ix = 0; ix < 8; ix++) {
+							for (int iy = 0; iy < 8; iy++) {
+								myGdiplusGame::drawTile(graphBackBuffer, 
+									&imgBasicTile, 
+									ix, iy ,
+									g_MapRoom1);
+							}
+						}
+
+						
+						graphBackBuffer->DrawImage(&imgPlayer, 
+							Rect((g_nPlayerXPos * 16) - 16 + 8, (g_nPlayerYPos * 16) -16 + 8, 32, 32),//대상위치 // - 16는 정중앙으로 그리고 +8은 타일 보정 뒤에 32 32는 총 크기
+							0, 64*2, 64, 64,//원본 위치
+							UnitPixel
+						);
+
+						graphics.ScaleTransform(2, 2);
+						graphics.DrawImage(&bmpMem, rectScreen);
+						graphics.ResetTransform();
+					}
+					ReleaseDC(msg.hwnd, hdc);
+				}
+				break;
+				default:
+					break;
+				}
+
+
+			}
+		}
+	}
+
+	//--------------------------------------
+	//gdi plus 종료코드 
+	GdiplusShutdown(gdiplusToken);
+	//--------------------------------------
+
+
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_EXAM8_EXIT_ROOM, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 응용 프로그램 초기화를 수행합니다.
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EXAM8_EXIT_ROOM));
+
+    MSG msg;
+
+
+	// 기본 메시지 루프입니다.
+	GDIPLUS_Loop(msg);
+
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  목적: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_EXAM8_EXIT_ROOM));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_EXAM8_EXIT_ROOM);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   설명:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  목적:  주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+	case WM_KEYDOWN:
+	{
+		switch (wParam) 
+		{
+		case VK_UP:
+			
+			if (g_nPlayerYPos >= 0) g_nPlayerYPos--;
+			
+			break;
+		case VK_DOWN:
+			if (g_nPlayerYPos < 7) g_nPlayerYPos++;
+			break;
+		case VK_LEFT:
+			if (g_nPlayerXPos > 0) g_nPlayerXPos--;
+			
+			break;
+		case VK_RIGHT:
+			if (g_nPlayerXPos < 7) g_nPlayerXPos++;
+			
+			break;
+
+		}
+	}
+		break;
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다.
+            switch (wmId)
+            {
+			case IDM_APP_START:
+				g_dwGdiLoopFsm = 10;
+				break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다.
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
