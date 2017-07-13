a66c0ad20bdcc10716faeca8974320a494815422
kujin <dktmzdf@gmail.com>
2017-07-13 14:43:08 +0900

    exam4

---
+++ day17/exam4/gameObject.h
@@ -0,0 +1,13 @@
+#pragma once
+
+struct GameObject
+{
+	irr::core::vector2df m_vPosition;
+	irr::f64 m_fRotation;
+	irr::f64 m_fSpeed;
+
+	Image *m_pImg;
+};
+
+extern void GameObject_Apply(GameObject *pThis, irr::f64 fDelta);
+extern void GameObject_Draw(GameObject *pThis, Graphics &grp);
